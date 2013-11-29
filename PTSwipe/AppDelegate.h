b2dc4f24eb00d7187aceae1a20b42a3f9a563e7c
Phillip Harris <philliptharris@gmail.com>
2013-11-29 10:06:35 -0500

    Initial Commit

---
+++ PTSwipe/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  PTSwipe
+//
+//  Created by Phillip Harris on 11/29/13.
+//  Copyright (c) 2013 Phillip Harris. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ PTSwipe/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  PTSwipe
+//
+//  Created by Phillip Harris on 11/29/13.
+//  Copyright (c) 2013 Phillip Harris. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
